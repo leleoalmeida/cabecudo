54e9b4b23c6b38735f25eccefd462e7d7594606d
Leonardo Almeida <leo@leleo.com.br>
2020-12-19 17:50:08 -0300

    init

---
+++ code.js
@@ -0,0 +1,11 @@
+function check() {
+    entrada = document.querySelector(".entrada").value
+    console.log(entrada)
+    if (entrada == 41020191) {
+        location.href = './video.html'
+    }
+    if (entrada == '35p31h0 m070') {
+        location.href = './video.html'
+    }
+
+}
